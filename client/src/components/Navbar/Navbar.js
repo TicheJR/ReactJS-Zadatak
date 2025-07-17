c247fef4ad1ae3c4534537918bcc9cc30989aeee
TicheJR <rajkovicpetar998@gmail.com>
2025-07-17 09:45:40 +0200

    Logout

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,11 +2,24 @@ import { useState } from 'react';
 import { FaUserCircle } from 'react-icons/fa';
 import { FiLogOut } from 'react-icons/fi';
 import './Navbar.css';
+import { logout } from '../../store/authSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
-function Navbar({ user, onLogout }) {
+function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
 
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const user = useSelector((state) => state.auth.user)
+
   const toggleMenu = () => setMenuOpen(!menuOpen);
+
+  const handleLogout = () => {
+    dispatch(logout());
+    navigate('/login');
+  }
 
   return (
     <div className="navbar">
@@ -16,11 +29,11 @@ function Navbar({ user, onLogout }) {
       <div className="navbar-right">
         <div className="profile" onClick={toggleMenu}>
           <FaUserCircle size={28} />
-          <span>{user.username}</span>
+          <span>{user?.username}</span>
         </div>
         {menuOpen && (
           <div className="dropdown">
-            <button onClick={onLogout}>
+            <button onClick={handleLogout}>
               <FiLogOut /> Logout
             </button>
           </div>
